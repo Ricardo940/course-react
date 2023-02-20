2bc10c10495aacd8df1d2a4f44f5f3adcbe585a1
ricardo940 <electronica0506@gmail.com>
2023-02-19 21:21:50 -0500

    change BroswerRouter to HashRouter

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Routes, Route} from "react-router-dom";
 import Layout from '@containers/Layout';
 import Home from '@pages/Home';
 import Login from '@pages/Login';
@@ -21,7 +21,7 @@ const App = () => {
     const initialState = useInitialState();
     return(
         <AppContext.Provider value={initialState}>
-            <BrowserRouter>
+            <HashRouter>
                 <Layout>
                     <Routes>
                         <Route path="/" element={<Home />} />
@@ -36,7 +36,7 @@ const App = () => {
                         <Route path="*" element={<NotFound />} />
                     </Routes>
                 </Layout>
-            </BrowserRouter>
+            </HashRouter>
         </AppContext.Provider>
         
     );
